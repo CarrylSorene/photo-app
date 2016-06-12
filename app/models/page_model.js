b809780eb592f96fd60c70987b6c399e4c06868a
Carryl Sorene <carrylsorene@gmail.com>
2016-06-12 19:51:18 +0100

    Photo and Comment Models

--- app/models/page_model.js
+++ app/models/page_model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var PageSchema = new Schema({
-  name: {type: String, unique: true},
+  name: { type: String, unique: true },
   commentId: Schema.ObjectId
 });
 mongoose.model('Page', PageSchema);
